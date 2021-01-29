5fa2667720bb41a825f86a708f5bb6d3631c671a
Faiz <faizhassan550@gmail.com>
2021-01-29 21:00:45 +0500

    Login

--- FaceBookLogin/ViewController.swift
+++ FaceBookLogin/ViewController.swift
@@ -6,12 +6,25 @@
 //
 
 import UIKit
+import FBSDKLoginKit
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let loginButton = FBLoginButton()
+                loginButton.center = view.center
+                view.addSubview(loginButton)
+        loginButton.permissions = ["public_profile", "email"]
+        // Observe access token changes
+        // This will trigger after successfully login / logout
+        NotificationCenter.default.addObserver(forName: .AccessTokenDidChange, object: nil, queue: OperationQueue.main) { (notification) in
+            
+            // Print out access token
+            print("FB Access Token: \(String(describing: AccessToken.current?.tokenString))")
+        }
     }
 
 
